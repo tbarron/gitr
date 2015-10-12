5922125db7c29774b1e50c5945132b0ee3b055ac
Tom Barron <tom.barron@comcast.net>
2015-10-11 21:30:26 -0400

    Need test for 'pydoc gitr'
     - FIX: Wrote run() to make it easy to run a command and capture the
       output, wrote a test for 'pydoc gitr', docstring for gitr
    
    Version:   0.0.0.3

--- gitr/__init__.py
+++ gitr/__init__.py
@@ -1,5 +1,54 @@
 # -*- coding: utf-8 -*-
+"""git helpers
+
+gitr provides a collection of subcommands that I find useful in managing git
+repositories.
+
+Commands:
+    gitr dunn - will suggest what the next step to be done probably is based on
+        the state of the repo.
+
+    gitr depth - will report how far back a commitish is (number of commits
+        between the one in question and the present as well as the age of the
+        target committish)
+
+    gitr hook - will list available hooks (--list), install and link a hook
+        (--add), show a list of installed hooks (--show), and remove hooks
+        (--rm)
+
+    gitr bv - will bump the version of the current repo, assumed to be in
+        version.py
+
+    gitr flix - will find and report conflicts
+
+    gitr nodoc - will find and report any functions in the current tree in .py
+        files that have no docstring
+
+    gitr dupl - will find and report any duplicate functions in the current
+        tree in .py files
+
+Usage:
+    gitr dunn [(-d|--debug)]
+    gitr depth [(-d|--debug)] <commitish>
+    gitr hook [(-d|--debug)] (--list|--show)
+    gitr hook [(-d|--debug)] (--add|--rm) <hookname>
+    gitr bv [(-d|--debug)] [<part>]
+    gitr flix [(-d|--debug)] [<filename>]
+    gitr nodoc [(-d|--debug)]
+    gitr dupl [(-d|--debug)]
+    gitr (-h|--help|--version)
+
+Options:
+    -h --help        Provide help info (display this document)
+    --version        Show version
+    --list           List git hooks available to install
+    --show           List installed git hooks
+    --add            Add a hook by name
+    --rm             Remove a hook by name
+"""
+
+import version
 
 __author__ = 'Tom Barron'
 __email__ = 'tusculum@gmail.com'
-__version__ = '0.0.0'
+__version__ = version.__version__
--- tests/test_gitr.py
+++ tests/test_gitr.py
@@ -8,10 +8,14 @@ test_gitr
 Tests for `gitr` module.
 """
 import docopt
+import pexpect
 import pytest
+import shlex
+import subprocess
 import unittest
 
-from gitr import gitr
+import gitr
+from gitr import gitr as app
 
 
 # -----------------------------------------------------------------------------
@@ -36,6 +40,7 @@ def docopt_exp(**kw):
           '--list': False,
           '--show': False,
           '--rm': False,
+          'dupl': False
           }
     for k in kw:
         if k in rv:
@@ -44,16 +49,72 @@ def docopt_exp(**kw):
 
 
 # -----------------------------------------------------------------------------
-@pytest.mark.parametrize("argl", (["--help"],
-                                  ["--version"],
-                                  ["--debug"],
+def test_docopt_help(capsys):
+    """
+    Calling docopt.docopt() with '--help' in the arg list
+    """
+    pytest.dbgfunc()
+    exp = docopt_exp(help=True)
+    with pytest.raises(SystemExit):
+        r = docopt.docopt(gitr.__doc__, ['--help'])
+    o, e = capsys.readouterr()
+    for k in exp:
+        assert k in o
+
+
+# -----------------------------------------------------------------------------
+@pytest.mark.parametrize("argd", (({"--version": True}), 
+                                  ({"--debug": True, 'dunn': True}),
                                   ))
-def test_docopt_help(argl):
-    exp = docopt_exp()
-    r = docopt.docopt(gitr.__doc__, argl)
+def test_docopt(argd, capsys):
+    """
+    Test calls to docopt.docopt() that should be successful
+    """
+    pytest.dbgfunc()
+    exp = docopt_exp(**argd)
+    r = docopt.docopt(gitr.__doc__, argd.keys())
     assert r == exp
 
 
+# -----------------------------------------------------------------------------
+def test_pydoc_gitr():
+    """
+    Verify the behavior of running 'pydoc gitr'
+    """
+    pytest.dbgfunc()
+    r = run("pydoc gitr")
+    z = docopt_exp()
+    for k in z:
+        assert k in r
+
+
+# -----------------------------------------------------------------------------
+def run(cmd, input=None):
+    """
+    Run *cmd*, optionally passing string *input* to it on stdin, and return
+    what the process writes to stdout
+    """
+    try:
+        p = subprocess.Popen(shlex.split(cmd),
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        if input:
+            p.stdin.write(input)
+        (o, e) = p.communicate()
+        if p.returncode == 0:
+            rval = o
+        else:
+            rval = 'ERR:' + e
+    except OSError as e:
+        if 'No such file or directory' in str(e):
+            rval = 'ERR:' + str(e)
+        else:
+            raise
+
+    return rval
+
+# -----------------------------------------------------------------------------
 if __name__ == '__main__':
     import sys
     sys.exit(unittest.main())
