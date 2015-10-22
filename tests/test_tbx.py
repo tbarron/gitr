7c16d13789d351d37bd345e718053f2c8ccdfa5f
Tom Barron <tom.barron@comcast.net>
2015-10-21 21:14:41 -0400

    tbx.contents needs testing
     - FIX: add tests for contents as a list, as a string, failing because the
       file does not exist, and returning a default value
    
    Version:   0.0.0.24

--- tests/test_tbx.py
+++ tests/test_tbx.py
@@ -1,7 +1,9 @@
 import os
+import pytest
 
 from gitr import tbx
 
+# -----------------------------------------------------------------------------
 def test_chdir(tmpdir):
     """
     Test the chdir contextmanager
@@ -10,3 +12,56 @@ def test_chdir(tmpdir):
     with tbx.chdir(tmpdir.strpath):
         assert os.getcwd() == tmpdir.strpath
     assert os.getcwd() == here
+
+
+# -----------------------------------------------------------------------------
+@pytest.fixture
+def contents_setup(tmpdir):
+    """
+    set up data for test
+    """
+    pytest.this = {}
+    td = pytest.this['td'] = ["one", "two", "three", "four", "five"]
+    tfn = pytest.this['tfn'] = tmpdir.join("testthis")
+    tfn.write(''.join([x+"\n" for x in td]))
+
+
+# -----------------------------------------------------------------------------
+def test_contents_list(tmpdir, contents_setup):
+    """
+    Test getting the contents of a file
+    """
+    pt = pytest.this
+    c = tbx.contents(pt['tfn'].strpath, type=list)
+    assert c == [x + '\n' for x in pt['td']]
+
+
+# -----------------------------------------------------------------------------
+def test_contents_str(tmpdir):
+    """
+    Test getting the contents of a file
+    """
+    pt = pytest.this
+    c = tbx.contents(pt['tfn'].strpath)
+    assert c == ''.join([x + '\n' for x in pt['td']])
+
+
+# -----------------------------------------------------------------------------
+def test_contents_fail(tmpdir):
+    """
+    Test getting the contents of a file
+    """
+    pt = pytest.this
+    with pytest.raises(IOError) as e:
+        c = tbx.contents(pt['tfn'].strpath + "_nonesuch")
+    assert "No such file or directory" in str(e)
+
+
+# -----------------------------------------------------------------------------
+def test_contents_default(tmpdir):
+    """
+    Test getting the contents of a file
+    """
+    pt = pytest.this
+    c = tbx.contents(pt['tfn'].strpath + "_nonesuch", default="take this instead")
+    assert "take this instead" in c
